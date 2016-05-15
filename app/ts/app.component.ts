5236a65c502cd384aa10bdd6acbca8814b8e6523
Vicente Fernando Noriega <vincentnoriega9@gmail.com>
2016-05-15 04:18:38 +0000

    california

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -2,7 +2,11 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'blazter',
-    template: '<h1>Vicente Fernando Noriega</h1>'
+    template: '<h1>Vicente Noriega {{name}} and {{MyArray}}</h1>'
 })
 
-export class x {}
+export class x {
+    name = "chente1er";
+    MyArray = ["apples", "oranges", "melons"];
+    
+}
